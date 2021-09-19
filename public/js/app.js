31f7cec6bb8a266a3dc0b18570301c82580e1e22
Cristian Army <armycrih@gmail.com>
2021-09-19 16:04:43 -0300

    mejoras welcome

--- public/js/app.js
+++ public/js/app.js
@@ -39656,9 +39656,51 @@ var _hoisted_25 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNo
 
 var _hoisted_26 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)(" Register ");
 
-var _hoisted_27 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<main class=\"mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28\"><div class=\"sm:text-center md:text-center lg:text-left\"><img class=\"md:hidden h-24 w-24 justify-center\" src=\"/images/logo-datatuill.png\"><h1 class=\"text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl\"><span class=\"block xl:inline\">Datatuill</span></h1><h2><span class=\"block text-indigo-600 xl:inline\">The World&#39;s First Dummy Data Platform</span></h2><p class=\"mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0\"> Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua. </p><div class=\"mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start\"><div class=\"rounded-md shadow\"><a href=\"#\" class=\"w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10\"> Get started </a></div><div class=\"mt-3 sm:mt-0 sm:ml-3\"><a href=\"#\" class=\"w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10\"> Live demo </a></div></div></div></main>", 1);
+var _hoisted_27 = {
+  "class": "mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
+};
+var _hoisted_28 = {
+  "class": "sm:text-center md:text-center lg:text-left"
+};
 
-var _hoisted_28 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
+var _hoisted_29 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<img class=\"md:hidden px-28 w-auto\" src=\"/images/logo-data.png\"><h1 class=\"text-5xl tracking-tight font-extrabold text-gray-900 sm:text-6xl md:text-7xl\"><span class=\"block xl:inline\">Datatuill</span></h1><h2 class=\"text-3xl tracking-tight font-extrabold sm:text-4xl md:text-5xl\"><span class=\"block text-indigo-600 xl:inline\">The World&#39;s First Dummy Data Platform</span></h2><p class=\"mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0\"> Dataset with dummy data from over 70 industry examples </p>", 4);
+
+var _hoisted_33 = {
+  "class": "flex justify-center items-center"
+};
+var _hoisted_34 = {
+  "class": "bg-white p-4 w-5/6 rounded-md"
+};
+var _hoisted_35 = {
+  "class": "mt-10 mb-0 border-2 py-1 px-3 flex justify-between rounde-md rounded-md"
+};
+
+var _hoisted_36 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("input", {
+  "class": "flex-grow border-0 outline-none text-gray-600 focus:text-gray-600",
+  type: "text",
+  placeholder: "Search Dataset..."
+}, null, -1
+/* HOISTED */
+);
+
+var _hoisted_37 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("svg", {
+  xmlns: "http://www.w3.org/2000/svg",
+  "class": "h-6 w-6 text-gray-400 hover:text-indigo-400 transition duration-100 cursor-pointer",
+  fill: "none",
+  viewBox: "0 0 24 24",
+  stroke: "currentColor"
+}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("path", {
+  "stroke-linecap": "round",
+  "stroke-linejoin": "round",
+  "stroke-width": "2",
+  d: "M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+})], -1
+/* HOISTED */
+);
+
+var _hoisted_38 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"flex flex-wrap justify-center mt-1 sm:mt-3\"><div class=\"w-40 sm:mt-3 my-1 mx-1\"><a href=\"#\" class=\"w-full flex items-center justify-center px-8 py-3 md:py-4 border border-transparent text-base font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-700\"> Social Media </a></div><div class=\"w-40 sm:mt-3 my-1 mx-1\"><a href=\"#\" class=\"w-full flex items-center justify-center px-8 py-3 md:py-4 border border-transparent text-base font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-700\"> E-Commerce </a></div><div class=\"w-40 sm:mt-3 my-1 mx-1\"><a href=\"#\" class=\"w-full flex items-center justify-center px-8 py-3 md:py-4 border border-transparent text-base font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-700\"> Gaming </a></div><div class=\"w-40 sm:mt-3 my-1 mx-1\"><a href=\"#\" class=\"w-full flex items-center justify-center px-8 py-3 md:py-4 border border-transparent text-base font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-700\"> Argentina </a></div><div class=\"w-40 sm:mt-3 my-1 mx-1\"><a href=\"#\" class=\"w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4\"> Live demo </a></div></div>", 1);
+
+var _hoisted_39 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
   "class": "lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2"
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("img", {
   "class": "h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full",
@@ -39668,7 +39710,7 @@ var _hoisted_28 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElement
 /* HOISTED */
 );
 
-var _hoisted_29 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"flex justify-center mx-4 mt-4 sm:items-center sm:justify-between\"><div class=\"text-center text-sm text-gray-500 sm:text-left\"><div class=\"flex items-center\"><svg fill=\"none\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" viewBox=\"0 0 24 24\" stroke=\"currentColor\" class=\"-mt-px w-5 h-5 text-gray-400\"><path d=\"M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z\"></path></svg><a href=\"https://laravel.bigcartel.com\" class=\"ml-1 underline\"> Shop </a><svg fill=\"none\" stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" viewBox=\"0 0 24 24\" class=\"ml-4 -mt-px w-5 h-5 text-gray-400\"><path d=\"M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z\"></path></svg><a href=\"https://github.com/sponsors/taylorotwell\" class=\"ml-1 underline\"> Sponsor </a></div></div><div class=\"ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0\"> © 2021 All Rights Reserved, Datatuill </div></div>", 1);
+var _hoisted_40 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"flex justify-center mx-4 mt-4 sm:items-center sm:justify-between\"><div class=\"text-center text-sm text-gray-500 sm:text-left\"><div class=\"flex items-center\"><svg fill=\"none\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" viewBox=\"0 0 24 24\" stroke=\"currentColor\" class=\"-mt-px w-5 h-5 text-gray-400\"><path d=\"M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z\"></path></svg><a href=\"https://laravel.bigcartel.com\" class=\"ml-1 underline\"> Shop </a><svg fill=\"none\" stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" viewBox=\"0 0 24 24\" class=\"ml-4 -mt-px w-5 h-5 text-gray-400\"><path d=\"M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z\"></path></svg><a href=\"https://github.com/sponsors/taylorotwell\" class=\"ml-1 underline\"> Sponsor </a></div></div><div class=\"ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0\"> © 2021 All Rights Reserved, Datatuill </div></div>", 1);
 
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   var _component_Head = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("Head");
@@ -39684,6 +39726,8 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
   var _component_PopoverPanel = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("PopoverPanel");
 
   var _component_Popover = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("Popover");
+
+  var _component_spa = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("spa");
 
   return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_Head, {
     title: "Datatuill"
@@ -39846,7 +39890,14 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
     _: 1
     /* STABLE */
 
-  }), _hoisted_27])]), _hoisted_28]), _hoisted_29], 64
+  }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("main", _hoisted_27, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_28, [_hoisted_29, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_33, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_34, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_35, [_hoisted_36, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_spa, null, {
+    "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+      return [_hoisted_37];
+    }),
+    _: 1
+    /* STABLE */
+
+  })])])]), _hoisted_38])])])]), _hoisted_39]), _hoisted_40], 64
   /* STABLE_FRAGMENT */
   );
 }
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -88,24 +88,58 @@
         </Popover>
 
         <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-          <div class="  sm:text-center md:text-center lg:text-left">
-            <img class="md:hidden  h-24 w-24 justify-center " src="/images/logo-datatuill.png"  />
-            <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+          <div class=" sm:text-center  md:text-center lg:text-left">
+           
+              
+              <img class="  md:hidden px-28  w-auto " src="/images/logo-data.png"  />
+              
+            
+            <h1 class="text-5xl tracking-tight font-extrabold text-gray-900 sm:text-6xl md:text-7xl">
               <span class="block xl:inline">Datatuill</span>
-            </h1><h2>  
+            </h1><h2 class="text-3xl tracking-tight font-extrabold  sm:text-4xl md:text-5xl">  
               <span class="block text-indigo-600 xl:inline">The World's First Dummy Data Platform</span>
             </h2>
             <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+              Dataset with dummy data from over 70 industry examples
             </p>
-            <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-              <div class="rounded-md shadow">
-                <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
-                  Get started
-                </a>
+
+  
+            <div class=" flex justify-center items-center">
+            <div class="bg-white p-4 w-5/6 rounded-md">
+              <div class="mt-10 mb-0 border-2 py-1 px-3 flex justify-between rounde-md rounded-md">
+                <input class="flex-grow border-0 outline-none text-gray-600 focus:text-gray-600 " type="text" placeholder="Search Dataset..." />
+                <spa>
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400 hover:text-indigo-400 transition duration-100 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                  </svg>
+                </spa>
               </div>
-              <div class="mt-3 sm:mt-0 sm:ml-3">
-                <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+            </div>
+            </div>
+
+            <div class=" flex flex-wrap justify-center mt-1 sm:mt-3  ">
+              <div class=" w-40  sm:mt-3 my-1 mx-1">
+                <a href="#" class="w-full flex items-center justify-center px-8 py-3 md:py-4 border border-transparent text-base font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-700 ">
+                  Social Media
+                </a>
+              </div> 
+              <div class="w-40 sm:mt-3 my-1 mx-1">
+                <a href="#" class="w-full flex items-center justify-center px-8 py-3 md:py-4 border border-transparent text-base font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-700 ">
+                  E-Commerce
+                </a>
+              </div> 
+              <div class="w-40 sm:mt-3 my-1 mx-1">
+                <a href="#" class="w-full flex items-center justify-center px-8 py-3 md:py-4 border border-transparent text-base font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-700 ">
+                  Gaming
+                </a>
+              </div> 
+              <div class="w-40 sm:mt-3 my-1 mx-1">
+                <a href="#" class="w-full flex items-center justify-center px-8 py-3 md:py-4 border border-transparent text-base font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-700 ">
+                  Argentina
+                </a>
+              </div>              
+              <div class="w-40 sm:mt-3  my-1 mx-1">
+                <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4">
                   Live demo
                 </a>
               </div>
